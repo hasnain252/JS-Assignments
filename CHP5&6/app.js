b7e57a1c9029c0a2108f77dc36df99a81a0f81bc
hasnain252 <121338205+hasnain252@users.noreply.github.com>
2024-02-29 13:33:47 +0500

    chp 5&6

---
+++ CHP5&6/app.js
@@ -0,0 +1,100 @@
+// q1
+
+var x = 25
+var y = 7
+
+console.log(x % y)
+
+
+// q2
+var a = 15
+var b = 4
+
+a= 15+10
+b = 4-2
+
+console.log(a*b)
+
+// q3
+var c = 12
+var d = 3
+
+console.log(c ** d)
+
+// q4
+
+var e = 10
+var f = 3
+
+console.log(e += f * 2)
+
+// q5
+
+var g = 20
+var h = 4
+
+console.log(g/(h+5))
+
+// q6
+var i = 8
+var j = 3
+
+console.log((i-2)* j)
+
+// q7
+var k = 18
+var l = 5
+
+console.log(k - l + 2)
+
+// q8
+
+var m = 7
+var n = 2
+
+console.log(m ** n)
+
+// q9
+var o = 13
+var p = 4
+
+console.log(o % (p + 2))
+
+// q10
+var q = 16
+var r = 3
+
+console.log((q/2)*r)
+
+// q11
+
+var s = 25
+var t = 3
+
+console.log((s-1)/t)
+
+// q12
+
+var u = 11
+var v = 3
+
+console.log(u-(v+2))
+
+// q13
+var w = 22
+var x = 5
+
+console.log(w + (x ** 2))
+
+// q14
+var y = 19
+var z = 4
+
+console.log(y % (z + 1))
+
+// q15
+
+var a1 = 10
+var b1 = 3
+
+console.log(a1 * (b1 - 2))
